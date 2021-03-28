107d17903aee3015f2292c4583b4b86ad04c87de
OB1-Jacobi <jpattimore@gmail.com>
2021-03-28 13:04:03 -0400

    On branch lidar2  Changes to be committed: unladen swallow 	modified:   snitch-sniffer.py

--- snitch-sniffer.py
+++ snitch-sniffer.py
@@ -1,6 +1,6 @@
 userName = input("What is your name? ")
 yourQuest = input("What is your quest? ")
-unladenSwallow = input("What is your favorite color? ")
+unladenSwallow = input("What is the airspeed velocity of an unladen swallow? ")
 while userName != "Clark Kent":
     print("You are not Superman - try again!")
     userName = input("What is your name? ")
